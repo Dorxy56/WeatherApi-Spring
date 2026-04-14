88a94cc5b725c99e2284b868eb34eec38e9cc62c
drxy <ddorian092@gmail.com>
2026-04-14 11:07:55 +0200

    VERY basic frontend and Cors changes to WeatherControler.java

--- Frontend/app.js
+++ Frontend/app.js
@@ -3,7 +3,7 @@ const datum = document.getElementById('datum');
 const button = document.getElementById('button');
 const result = document.getElementById('result');
 
-const baseUrl = 'https://localhost:8080/api/idojaras/';
+const baseUrl = 'http://localhost:8080/api/idojaras/';
 
 //CORS policy miatt kellene de beraktuk a mode: "no-cors"-t 
 /*const httpsHeaders = {
@@ -14,20 +14,49 @@ const myHeaders = new Headers(httpsHeaders);*/
 
 async function getWeather() {
 
-    if (!varos.value.trim() && !datum.value) {
-
-    } else if (!datum.value) {
-        let response = await fetch(baseUrl + '?varos=' + varos.value.trim(), {
+    if (!varos.value.trim() && !datum.value || !varos.value.trim() && datum.value) {
+        let response = await fetch(baseUrl, {
             headers: {
-                "Content-Type": "Application/json",
+                "Content-Type": "application/json",
                 "Access-Control-Allow-Origin": "*"
             }
         });
         console.log(response);
-        if (response.ok) {
-            let data = await response.json();
-            console.log(data);
 
+        let data = await response.json();
+        console.log(data);
+
+        result.innerHTML = JSON.stringify(data);
+    } else if (!datum.value) {
+        let response = await fetch(baseUrl + '?varos=' + varos.value.trim(), {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }
+        });
+        console.log(response);
+
+        let data = await response.json();
+        console.log(data);
+
+        result.innerHTML = JSON.stringify(data);
+    } else {
+        let response = await fetch(baseUrl + '?varos=' + varos.value.trim() + '?datum=' + datum.value, {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }
+        });
+        console.log(response);
+
+        let data = await response.json();
+        console.log(data);
+
+        result.innerHTML = JSON.stringify(data);
+
+        if (Array.isArray(data)) {
+            result.innerHTML = JSON.stringify(data[0]);
+        } else {
             result.innerHTML = JSON.stringify(data);
         }
     }
--- idojarasapi/src/main/java/hu/pelda/idojarasapi/Controller/WeatherController.java
+++ idojarasapi/src/main/java/hu/pelda/idojarasapi/Controller/WeatherController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/idojaras")
 public class WeatherController {
